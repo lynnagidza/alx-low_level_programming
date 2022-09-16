ef5ee0905585a8cfad10a2b3359b56fd837ac86d
Agidza <lynnagidza@gmail.com>
2022-09-16 08:36:25 -0700

    new files

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,8 +1,10 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-#include <stdio.h>
-
+#ifndef MAIN_H                                                                                                                                                                                
+#define MAIN_H                                                                                                                                                                                
+                                                                                                                                                                                              
+#include <stdio.h>                                                                                                                                                                            
+                                                                                                                                                                                              
 void positive_or_negative(int i);
-
+int largest_number(int a, int b, int c);
+print_remaining_days();                                                                                                                                                             
+                                                                                                                                                                                              
 #endif /* MAIN_H */
