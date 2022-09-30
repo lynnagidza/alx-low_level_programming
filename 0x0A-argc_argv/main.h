e9a1b583a62dfe8ca7bfbc57cd3d6fc4522accd1
Agidza <lynnagidza@gmail.com>
2022-09-30 08:09:42 -0700

    all tasks

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * File: main.h
+ * Auth: Lynn Ekalaji
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x0A-argc_argv directory.
+ */
+
+int _putchar(char c);
+
+#endif
