5a1bac515fbcc0567d96c5f72425a473ace0aba1
lynnagidza <lynnagidza@gmail.com>
2023-02-24 18:59:44 +0300

    remainder files

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 
 /**
- * main - check the code for Holberton School students.
  * @argc: argument count.
  * @argv: argument vector.
  *
@@ -10,30 +9,30 @@
  */
 int main(int argc, char *argv[])
 {
-char *opc = (char *) main;
-int i, nbytes;
+    char *opc = (char *)main;
+    int i, nbytes;
 
-if (argc != 2)
-{
-printf("Error\n");
-exit(1);
-}
+    if (argc != 2)
+    {
+        printf("Error\n");
+        exit(1);
+    }
 
-nbytes = atoi(argv[1]);
+    nbytes = atoi(argv[1]);
 
-if (nbytes < 0)
-{
-printf("Error\n");
-exit(2);
-}
+    if (nbytes < 0)
+    {
+        printf("Error\n");
+        exit(2);
+    }
 
-for (i = 0; i < nbytes; i++)
-{
-printf("%02x", opc[i] & 0xFF);
-if (i != nbytes - 1)
-printf(" ");
-}
+    for (i = 0; i < nbytes; i++)
+    {
+        printf("%02x", opc[i] & 0xFF);
+        if (i != nbytes - 1)
+            printf(" ");
+    }
 
-printf("\n");
-return (0);
+    printf("\n");
+    return (0);
 }
---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+'''Module that holds the function def island_perimeter'''
+
+
+def island_perimeter(grid):
+    '''Finds the perimeter of an island'''
+
+    if not isinstance(grid, list):
+        return 0
+    p = 0
+
+    for row in range(len(grid)):
+        for col in range(len(grid[row])):
+            if grid[row][col] == 1:
+                p += 4
+                if row > 0 and grid[row - 1][col] == 1:
+                    p -= 1
+                if col > 0 and grid[row][col - 1] == 1:
+                    p -= 1
+                if col < (len(grid[row]) - 1) and grid[row][col + 1] == 1:
+                    p -= 1
+                if row < (len(grid) - 1) and grid[row + 1][col] == 1:
+                    p -= 1
+    return 
